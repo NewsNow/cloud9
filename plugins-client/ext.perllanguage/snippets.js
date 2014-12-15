1cbe3b8f282d620a9a6f8474bf56260f274adefc
Struan Bartlett <struan.bartlett@NewsNow.co.uk>
2014-12-15 13:07:27 +0000

    Improved sample Perl snippets

--- plugins-client/ext.perllanguage/snippets.js
+++ plugins-client/ext.perllanguage/snippets.js
@@ -1,8 +1,9 @@
 define(function(require, exports, module) {
 
 module.exports = {
-    "sub": "sub ^^ {\n\t\n}\n",
-    "if": "if( ^^ ) {\n\t\n}\n"
+   "sub": "sub ^^ {\n\t\n}\n",
+   "if": "if( ^^ ) {\n\t\n}\n",
+   "ife": "if( ^^ ) {\n\t\n}\nelse {\n\t\n}\n"
 };
 
 });
